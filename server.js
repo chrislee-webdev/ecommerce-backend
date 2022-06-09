e3f9d5eadda5dcd138cb1a3ea4bc1b8bde49cf91
Christopher <leechrs1@gmail.com>
2022-06-09 17:53:41 -0500

    stil l can't figure out what is wrong - can't get the server to start

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { sequelize } = require('./model');
+const { sequelize } = require('./models');
 const routes = require('./routes/api');
 // import sequelize connection
 
