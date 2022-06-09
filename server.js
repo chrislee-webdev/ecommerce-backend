594d5aa6856eeeb89be2ed3da15c5b225c978b2a
Christopher <leechrs1@gmail.com>
2022-06-09 17:07:14 -0500

    only three files show up inthe list of errors instead of four

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { sequelize } = require('./models/Product');
-const routes = require('./routes');
+const { sequelize } = require('./models');
+const routes = require('./routes/api');
 // import sequelize connection
 
 const app = express();
